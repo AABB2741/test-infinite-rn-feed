5636ffafed6e8dc352b5386c8d9150d331ad9e8e
Mario <mariosantos88477364484@gmail.com>
2024-11-08 23:24:44 -0300

    fix: video aspect ratio

--- src/features/posts/components/post-renderer/video/index.tsx
+++ src/features/posts/components/post-renderer/video/index.tsx
@@ -1,6 +1,6 @@
 import { ResizeMode, Video } from "expo-av";
-import { memo } from "react";
-import { Text } from "react-native";
+import { memo, useState } from "react";
+import { Text, View } from "react-native";
 
 import type { VideoPost } from "@/schemas/post/video";
 
@@ -14,23 +14,27 @@ interface VideoPostRendererProps extends VideoPost {
 export const VideoPostRenderer = memo<VideoPostRendererProps>(
   ({ id, author, videoUrl, isVisible, ...interactions }) => {
     console.log(`Rendering video: ${id.substring(0, 5)}`);
+    const [videoAspectRatio, setVideoAspectRatio] = useState<number>();
 
     return (
-      <>
+      <View style={styles.container}>
         <Text style={styles.contentAuthor}>
           {author.name} ({id.substring(0, 5)})
         </Text>
         <Video
           source={{ uri: videoUrl }}
           resizeMode={ResizeMode.CONTAIN}
-          style={styles.video}
+          style={[styles.video, { aspectRatio: videoAspectRatio }]}
           shouldPlay={isVisible}
           positionMillis={0}
           useNativeControls
           isLooping
+          onReadyForDisplay={({ naturalSize }) => {
+            setVideoAspectRatio(naturalSize.width / naturalSize.height);
+          }}
         />
         <PostControls.Interactions {...interactions} />
-      </>
+      </View>
     );
   },
 );
--- src/features/posts/components/post-renderer/video/styles.ts
+++ src/features/posts/components/post-renderer/video/styles.ts
@@ -1,9 +1,18 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    gap: 24,
+    overflow: "hidden",
+  },
   contentAuthor: {},
   video: {
     width: "100%",
-    flex: 1,
+    minHeight: 1,
+    flexShrink: 1,
   },
 });
